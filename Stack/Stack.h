93671dc0aaeefca3b359ec39333610309f92f747
e-chan-belov <echanbelov@icloud.com>
2024-08-29 17:15:21 +0300

    first constuct

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -2,17 +2,33 @@
 #define Stack_h
 
 template <class T>
-struct Zone {
-    struct Zone *next;
+class Zone {
+public:
+    Zone *next;
     T memory;
 };
 
 template <class T>
 class Stack {
 private:
-    
+    int size;
+    Zone<T> *root;
+public:
+    Stack(int size_ = 1);
     
 };
+
+template <class T>
+Stack<T>::Stack(int size_) : size(size_)  {
+    if (size_ < 1) { }
+    root = new Zone<T>;
+    Zone<T> *cur = root;
+    for (int i = 1; i < size; i++) {
+        cur->next = new Zone<T>;
+        cur = cur->next;
+    }
+    cur->next = NULL;
+}
 
 
 #endif /* Stack_h */
--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include "Stack.h"
 
 int main(int argc, const char * argv[]) {
-    
+    Stack<int> A;
     
     return 0;
 }
