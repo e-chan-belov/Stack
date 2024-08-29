c8d7decd2d6ea27fee84e4959864634f8919f277
e-chan-belov <echanbelov@icloud.com>
2024-08-29 16:26:36 +0300

    Initial Commit

---
+++ Stack/Stack.h
@@ -0,0 +1,18 @@
+#ifndef Stack_h
+#define Stack_h
+
+template <class T>
+struct Zone {
+    struct Zone *next;
+    T memory;
+};
+
+template <class T>
+class Stack {
+private:
+    
+    
+};
+
+
+#endif /* Stack_h */
